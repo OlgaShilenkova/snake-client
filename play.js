73e67cf093b304a1e8b1ad92d864af13d4856ad0
Olga Shilenkova <Olga1shilenkova@gmail.com>
2021-12-01 17:22:10 -0600

    add handleUserInput and setupInput functions and key \u0003(ctl-c)  for exit

--- play.js
+++ play.js
@@ -1,6 +1,26 @@
 
-const connect = require ("./client.js")
+const connect = require("./client.js")
 
 // console.log(`${connect}`);
 
+// setup interface to handle user input from stdin
 
+const handleUserInput = function (key) {
+  // your code here
+  // process.stdout.write('.');
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
